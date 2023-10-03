641f0cb339a28263ab719b5193dee63380ad2c21
Jörg von Kügelgen-Hempeler <joerg@jvkh.de>
2023-10-03 22:04:58 +0200

    First Draft with sample program

---
+++ client-data/Data/Infos/Extensions.md
@@ -0,0 +1,13 @@
+# Extensions
+
+### 1. Übersetzung
+German Language Pack
+MS-CEINTL
+
+### 2. Debugging
+PHP Debug
+xdebug
+
+### 3. PHP-Hilfen
+PHP Intelephense
+bmewburn
---
+++ client-data/Data/Vorlagen/Addition.php
@@ -0,0 +1,8 @@
+<?php
+echo 'Hallo'.PHP_EOL;
+echo 'Dieses Programm addiert zwei Zahlen'.PHP_EOL;
+$ersteZahl = 3;
+$zweiteZahl = 5;
+$ergebnis = $ersteZahl + $zweiteZahl;
+echo  'Das Ergebnis ist "'.$ergebnis.'".'.PHP_EOL;
+echo "Programmende".PHP_EOL;
